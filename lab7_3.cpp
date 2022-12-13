eced7561bd706d119c673d3f08ba97c3e6227c29
Nomsod <anuwatch6552@gmail.com>
2022-12-13 16:21:48 +0700

    lab7_3 succsesrwq

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,15 +1,40 @@
 #include<iostream>
+#include<cmath>
 
 using namespace std;
 
+double adiff(double A,double B){
+    while(A >360 or A < -360){
+        if(A > 360){
+            A = A-360;
+        }
+        else{
+            A = A+360;
+        }
+    }
+    while(B > 360 or B < -360){
+        if(B > 360){
+            B = B-360;
+        }
+        else{
+            B = B+360;
+        }
+    }
+    if ((360 - abs(A-B)) > abs(A-B)) {
+    double Y = abs(A-B);
+    }
+    else {
+    double Y = 360-abs(A-B);
+    }
+}
 
 int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+  cout << adiff(180,270) << "\n";
+  cout << adiff(210,45) << "\n";
+  cout << adiff(0,360) << "\n";
+  cout << adiff(10,350) << "\n";
+  cout << adiff(95,260) << "\n";
+  cout << adiff(90,-90) << "\n";
+  cout << adiff(1000,280) << "\n";
+  cout << adiff(60,244) << "\n";
 }
