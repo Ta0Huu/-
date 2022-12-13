b05244eb2a529446112cde180e9ec677cd056139
Nomsod <anuwatch6552@gmail.com>
2022-12-13 16:41:58 +0700

    lab7_4 siodwppq

--- lab7_4.cpp
+++ lab7_4.cpp
@@ -8,8 +8,27 @@ int main(){
 	string name[] = {"Luffy","Zoro","Sanji","Nami","Naruto","Sakura","All Might","Saitama","Eren","Mikasa"};
 	int id[] = {123,196,225,329,420,451,515,666,690,787};
 	double gpa[] = {1.50,2.12,4.00,3.69,2.50,2.76,3.54,2.14,3.50,1.88};
+	
+	float o;
+	int i;
+	cout << "Please input GPA: ";
+	cin >> o;
+	int L = 10;
+	
+	while (i < L){
+	    if (gpa[i] >= o){
+	        cout << id[i] << " " << name[i] << " " <<  gpa[i] << "\n";
+	    }
+	else{
 
-    //Write your code here.
-		
+	}
+	
+	
+	i=i+1;
+	}
+	
+
+
+
 	return 0;
 }
